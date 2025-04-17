f77cdab76c4e6ff599c31ff62f8d62fe071a6257
Noémie Gaudou <gaudou.noemie@gmail.com>
2025-04-17 18:08:07 +0200

    Mise en place de @input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
-
+import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    FaceSnapComponent
-  ],
+  imports: [FaceSnapComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+  myMorning!: FaceSnap;
+  myAnimal!: FaceSnap;
+
+  ngOnInit(): void {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      'Mon meilleur ami depuis toujours !',
+      new Date(),
+      15
+    );
+
+    this.myOtherSnap = new FaceSnap(
+      'Three Rock Mountain',
+      'https://st.depositphotos.com/1016676/2111/i/450/depositphotos_21117345-stock-photo-hikers.jpg',
+      'Un endroit magnifique pour les randonnées.',
+      new Date(),
+      23
+    );
+
+    this.myLastSnap = new FaceSnap(
+      'Un bon repas',
+      'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      'Mmmmh que c\'est bon !',
+      new Date(),
+      38
+    );
+    this.myMorning = new FaceSnap(
+      'Un magnifique Matin !',
+      'https://img.freepik.com/photos-gratuite/arrangement-boissons-chaudes-hiver_23-2149188011.jpg?semt=ais_hybrid&w=740',
+      'Tellement réconfortant un petit déjeuné au chaud !',
+      new Date(),
+      29
+    )
+
+    this.myAnimal = new FaceSnap(
+      'Mon bébé chien d\'amour',
+      'https://www.shutterstock.com/image-photo/playful-puppy-chewing-bone-on-600nw-2593367523.jpg',
+      'Mon adorable bébé chien avec son petit pull !',
+      new Date(),
+      58
+    )
+
+    
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,15 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './face-snap.component.scss'
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
+  @Input() faceSnap!: FaceSnap;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
+
 
   ngOnInit(): void {
-    this.title = 'Archibald';
-    this.description = 'Mon meilleur ami depuis toujours !';
-    this.createdAt = new Date();
-    this.snaps = 5;
+
+    this.snapButtonText = "Oh Snap !";
+    this.userHasSnapped = false
   }
+
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+    this.snap();
+    }
+  }
+
+
+  unSnap() {
+    this.faceSnap.removeSnap();
+    this.snapButtonText = 'Oh Snap !'
+    this.userHasSnapped = false;
+  }
+
+snap(){
+  this.faceSnap.addSnap();
+  this.snapButtonText ='Oops , unSnap!';
+  this.userHasSnapped = true;
+}
 }
