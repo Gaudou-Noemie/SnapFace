97f66b0a85fc04332f4841015421c8f67467057a
Noémie Gaudou <gaudou.noemie@gmail.com>
2025-04-29 15:52:06 +0200

    création de route avec id correspondante

--- src/app/services/face-snaps-service.ts
+++ src/app/services/face-snaps-service.ts
@@ -47,12 +47,15 @@ export class FaceSnapsService{
     getFaceSnaps(): FaceSnap[]{
         return [...this.faceSnaps];
     }
-    
-    snapFaceSnapById(faceSnapId: string, snapType: SnapType): void {
-        const foundFaceSnap = this.faceSnaps.find(FaceSnap => FaceSnap.id === faceSnapId);
+    getFaceSnapById(faceSnapId: string): FaceSnap {
+      const foundFaceSnap = this.faceSnaps.find(FaceSnap => FaceSnap.id === faceSnapId);
         if (! foundFaceSnap) {
             throw new Error('FaceSnap not found');
         }
-        foundFaceSnap.snap(snapType);
+          return foundFaceSnap
+    }
+    snapFaceSnapById(faceSnapId: string, snapType: SnapType): void {
+        const faceSnap = this.getFaceSnapById(faceSnapId);
+        faceSnap.snap(snapType);
     }
 }
