a04d81d6caf6e32762bd2841895f4a8e763e81a5
Noémie Gaudou <gaudou.noemie@gmail.com>
2025-05-02 16:37:18 +0200

    Création d'un formulaire et d'une écoute simple.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
 
-  constructor(private router: Router){}
+    userEmail!: string;
 
+  constructor(private router: Router){ }
+
+  ngOnInit(): void {
+    
+  }
   onContinue(){
     this.router.navigateByUrl('facesnaps');
+  }
+  onSubmitForm(): void {
+    console.log(this.userEmail);
   }
 }
