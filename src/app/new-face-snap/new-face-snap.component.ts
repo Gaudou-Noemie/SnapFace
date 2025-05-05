8412776020e63a714bb74f68c02e3e87d7a846a4
Noémie Gaudou <gaudou.noemie@gmail.com>
2025-05-05 16:02:10 +0200

    création du formulaire et récupération des valeurs en temps réelle

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,16 +1,26 @@
 
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { FaceSnap } from '../models/face-snap';
+import { AsyncPipe, DatePipe, NgIf, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-new-face-snap',
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule, 
+    NgIf,
+    AsyncPipe,
+    UpperCasePipe,
+    DatePipe,
+  ],
   templateUrl: './new-face-snap.component.html',
   styleUrl: './new-face-snap.component.scss'
 })
 export class NewFaceSnapComponent implements OnInit {
 
     snapForm!: FormGroup;
+    faceSnapPreview$!: Observable<FaceSnap>;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -21,6 +31,14 @@ export class NewFaceSnapComponent implements OnInit {
       imageUrl: [null],
       location: [null]
     });
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map((formValue: any) => ({
+        ...formValue,
+        createdDate: new Date(),
+        id:0,
+        snaps:0
+      }))
+    );
   }
 
   onSubmitForm(): void {
