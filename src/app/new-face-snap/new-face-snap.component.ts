3ecfc6c74dc1362dc69984b1dbeb7c3166a1d349
Noémie Gaudou <gaudou.noemie@gmail.com>
2025-05-05 15:02:39 +0200

    Mise en place de la nouvelle route pour la création de snap

---
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-face-snap',
+  imports: [],
+  templateUrl: './new-face-snap.component.html',
+  styleUrl: './new-face-snap.component.scss'
+})
+export class NewFaceSnapComponent {
+
+}
